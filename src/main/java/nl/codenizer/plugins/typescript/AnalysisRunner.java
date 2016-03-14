33538e8c4e90ea97cdc6790e3be72929e71cd465
Sander van Vliet <sander@codenizer.nl>
2016-03-14 19:13:47 +0100

    Call node analyzer.js

--- src/main/java/nl/codenizer/plugins/typescript/AnalysisRunner.java
+++ src/main/java/nl/codenizer/plugins/typescript/AnalysisRunner.java
@@ -10,6 +10,18 @@ public class AnalysisRunner {
     public AnalysisResult Execute() {
         AnalysisResult result = new AnalysisResult();
         
+       try {
+           // 1) Set working directory to projectRootDirectory
+           // 2) Call analyzer.js
+           // 3) Parse result file
+           Process p = Runtime.getRuntime().exec("node analyzer.js");
+           
+           p.waitFor();           
+       }
+       catch(Exception ex) {
+           
+       }
+    
         return result;
     }
 }
