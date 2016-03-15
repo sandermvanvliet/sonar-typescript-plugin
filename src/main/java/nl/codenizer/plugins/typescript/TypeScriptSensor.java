bb7fbb510c74828704736dd087b2f8d809d0430e
Sander van Vliet <sander@codenizer.nl>
2016-03-15 07:09:45 +0100

    Removed comments

--- src/main/java/nl/codenizer/plugins/typescript/TypeScriptSensor.java
+++ src/main/java/nl/codenizer/plugins/typescript/TypeScriptSensor.java
@@ -14,40 +14,20 @@ import org.sonar.api.measures.Measure;
 import org.sonar.api.resources.Project;
 
 public class TypeScriptSensor implements Sensor {
-    /**
-     * The file system object for the project being analysed.
-     */
     private final FileSystem fileSystem;
 
-    /**
-     * The logger object for the sensor.
-     */
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     public TypeScriptSensor(FileSystem fileSystem) {
         this.fileSystem = fileSystem;
     }
-
-    /**
-     * Determines whether the sensor should run or not for the given project.
-     *
-     * @param project the project being analysed
-     * @return always true
-     */
+    
     public boolean shouldExecuteOnProject(Project project) {
         // this sensor is executed on any type of project
         return true;
     }
     
-    /**
-     * Analyses project directory in search for IDE metadata configuration files
-     * and extracts relevant information.
-     *
-     * @param project       the project being analysed
-     * @param sensorContext the sensor context
-     */
     public void analyse(Project project, SensorContext sensorContext) {
-
         File rootDir = fileSystem.baseDir();
 
         log.info("Analysing project root in search for TypeScript files: " + rootDir.getAbsolutePath());
@@ -72,12 +52,7 @@ public class TypeScriptSensor implements Sensor {
         
         log.debug("measures saved");
     }
-
-    /**
-     * Returns the name of the sensor as it will be used in logs during analysis.
-     *
-     * @return the name of the sensor
-     */
+    
     public String toString() {
         return "TypeScriptSensor";
     }
