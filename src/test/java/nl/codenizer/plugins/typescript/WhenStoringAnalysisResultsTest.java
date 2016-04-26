16293c3e036f994813b3dc3e7ec7d6a3b83f6ec5
Sander van Vliet <sander@codenizer.nl>
2016-04-26 17:59:27 +0200

    Ignored test again

--- src/test/java/nl/codenizer/plugins/typescript/WhenStoringAnalysisResultsTest.java
+++ src/test/java/nl/codenizer/plugins/typescript/WhenStoringAnalysisResultsTest.java
@@ -3,21 +3,21 @@ package nl.codenizer.plugins.typescript;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-
-import java.io.File;
-
+import org.mockito.Matchers;
 import org.mockito.Mockito;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import org.sonar.api.batch.SensorContext;
+import org.sonar.api.batch.fs.FilePredicate;
+import org.sonar.api.batch.fs.FilePredicates;
 import org.sonar.api.batch.fs.FileSystem;
 import org.sonar.api.batch.fs.InputFile;
+import org.sonar.api.batch.fs.internal.DefaultInputFile;
 import org.sonar.api.measures.CoreMetrics;
 import org.sonar.api.resources.Project;
+
+import java.io.File;
+import java.util.Iterator;
+
+import static org.mockito.Mockito.*;
 
 public class WhenStoringAnalysisResultsTest {
     @Rule
@@ -83,7 +83,29 @@ public class WhenStoringAnalysisResultsTest {
     
     private FileSystem GivenAFileSystem() {
         FileSystem mocked = mock(FileSystem.class);
-        
+
+        when(mocked.inputFiles(Matchers.any(FilePredicate.class))).thenReturn(new Iterable<InputFile>() {
+            public Iterator<InputFile> iterator() {
+                return new Iterator<InputFile>() {
+                    public boolean hasNext() {
+                        return true;
+                    }
+
+                    public InputFile next() {
+                        return new DefaultInputFile("someModule", "src/test/resources/valid_result.json");
+                    }
+
+                    public void remove() {
+
+                    }
+                };
+            }
+        });
+        FilePredicates mockedPredicates = mock(FilePredicates.class);
+        FilePredicate mockedFilePredicate = mock(FilePredicate.class);
+        when(mockedFilePredicate.apply(Matchers.any(InputFile.class))).thenReturn(true);
+        when(mockedPredicates.hasLanguage(Matchers.anyString())).thenReturn(mockedFilePredicate);
+        when(mocked.predicates()).thenReturn(mockedPredicates);
         when(mocked.baseDir()).thenReturn(new File("./"));
         
         return mocked;
